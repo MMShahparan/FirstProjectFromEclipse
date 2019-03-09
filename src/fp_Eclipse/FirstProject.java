54c610cc6583d0695c0fa72a854c786ab114185d
MMShahparan <sparan27@gmail.com>
2019-03-09 00:31:17 -0500

    FP

---
+++ src/fp_Eclipse/FirstProject.java
@@ -0,0 +1,14 @@
+package fp_Eclipse;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("First line Display\n");
+		
+		System.out.println("2nd line Display in console \n");
+		
+	}
+
+}
